af96f78d7085e1322f296490df3e1c3c2471db5a
Paul Beeler <sparksco@gmail.com>
2012-09-05 00:40:25 -0600

    Add ThyrusHoloBlack Theme apk
    Signed-off-by: Paul Beeler <sparksco@gmail.com>

---
+++ ThyrusHoloBlack-vendor-blobs.mk
@@ -0,0 +1,6 @@
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.themeId = ThyrusHoloBlack \
+    persist.sys.themePackageName = com.theme.template.ThyrusHoloBlack
+
+PRODUCT_COPY_FILES += \
+    vendor/ThyrusHoloBlack/proprietary/app/ThyrusHoloBlack.apk:system/app/ThyrusHoloBlack.apk
---
+++ ThyrusHoloBlack-vendor.mk
@@ -0,0 +1,1 @@
+$(call inherit-product, vendor/ThyrusHoloBlack/ThyrusHoloBlack-vendor-blobs.mk)
