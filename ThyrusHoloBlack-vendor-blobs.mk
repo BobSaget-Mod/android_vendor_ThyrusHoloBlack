116640e80cbd35a80aad79a1064a86677f0a8b5c
Paul Beeler <sparksco@gmail.com>
2012-09-05 16:38:16 -0600

    ThyrusHoloBlack:
    Don't try to set as default theme it doesn't work
    
    Signed-off-by: Paul Beeler <sparksco@gmail.com>

--- ThyrusHoloBlack-vendor-blobs.mk
+++ ThyrusHoloBlack-vendor-blobs.mk
@@ -1,6 +1,2 @@
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.themeId = ThyrusHoloBlack \
-    persist.sys.themePackageName = com.theme.template.ThyrusHoloBlack
-
 PRODUCT_COPY_FILES += \
     vendor/ThyrusHoloBlack/proprietary/app/ThyrusHoloBlack.apk:system/app/ThyrusHoloBlack.apk
